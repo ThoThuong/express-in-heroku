1ece829b9033f70a6ce24fd3b911375f849872e1
TranNgocThuong-coder <Nathuong99@gmail.com>
2021-05-29 18:12:55 +0700

    ip

--- public/javascripts/script2.js
+++ public/javascripts/script2.js
@@ -680,7 +680,7 @@ $(function () {
                 $.ajax({
                     type: "POST",
                     headers: { 'Access-Control-Allow-Origin': '*' },
-                    url: 'http://127.0.0.1:5000/api/predict',
+                    url: 'http://17.41.143.61/api/predict',
                     data: dataToFlask,
                     contentType: false,
                     processData: false,
